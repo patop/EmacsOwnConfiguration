c9f94395acedfccd2858e2d5e2ab03c07ffd2396
Patricio Campillay <campillaypatricio46@gmail.com>
2023-09-22 16:55:13 -0300

    Update init.el

--- init.el
+++ init.el
@@ -2,37 +2,92 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;; Instalación de LSP (Language Server Protocol) y Company Mode
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Instalación de LSP (Language Server Protocol) 
 (unless (package-installed-p 'lsp-mode)
+  (package-refresh-contents)
   (package-install 'lsp-mode))
 
-(unless (package-installed-p 'company)
-  (package-install 'company))
+;; Configuración para usar Conda en Emacs
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
 
-;; Configuración de LSP para un lenguaje específico
-(require 'lsp-mode)
+;; Establece la ruta del ejecutable de Python de Conda
+(setq python-shell-interpreter "C:/Users/liuka/miniconda3/envs/spyder-env/python.exe")
+;; Establece la ruta de activate script de Conda
+(setq python-shell-args '("--init-file" "c:/Users/liuka/miniconda3/Scripts/activate" "C:/Users/liuka/miniconda3/envs/spyder-env/python.exe"))
+;;configuracion de python
+(setenv "PATH" (concat "C:/Users/liuka/miniconda3/envs/spyder-env/python.exe" (getenv "PATH")))
+(setq exec-path (append '("C:/Users/liuka/miniconda3/envs/spyder-env/python.exe") exec-path))
+;; Establecer la ruta al intérprete de Python de Miniconda
+(setq python-shell-interpreter "C:/Users/liuka/miniconda3/envs/spyder-env/python.exe")
+;; Agregar la ruta de los scripts de Miniconda al PATH de Emacs
+(setq exec-path (append '("C:/Users/liuka/miniconda3/Scripts") exec-path))
+;; Configurar el shell interactivo de Python
+(setq python-shell-interpreter-args "-i")
+
+(defun run-python-file ()
+  "Ejecutar el archivo Python activo."
+  (interactive)
+  (let ((python-script (buffer-file-name)))
+    (shell-command (concat "python " python-script))))
+
+;; Asignar un atajo (por ejemplo, F5) para ejecutar el archivo Python
+(global-set-key (kbd "<f5>") 'run-python-file)
+
+;;tema del completado
+(defun lsp--sort-completions (completions)
+  (lsp-completion--sort-completions completions))
+
+(defun lsp--annotate (item)
+ (lsp-completion--annotate item))
+
+(defun lsp--resolve-completion (item)
+ (lsp-completion--resolve item))
+
+;; Definir una función para determinar el identificador de lenguaje para emacs-lisp-mode
+(defun my-lsp-emacs-lisp-language-id (_workspace _params)
+  "Return the language ID for emacs-lisp-mode."
+  "emacs-lisp")
+
+;; Configurar lsp-language-id-configuration para emacs-lisp-mode
+(setq lsp-language-id-configuration
+      '((emacs-lisp-mode . "emacs-lisp")))
+
+;; Configurar la función para el modo emacs-lisp
+(setq lsp-language-id-configuration
+      (append lsp-language-id-configuration
+              '((emacs-lisp-mode . my-lsp-emacs-lisp-language-id))))
+
+;; Definir una función para determinar el identificador de lenguaje para python-mode
+(defun my-lsp-python-language-id (_workspace _params)
+  "Return the language ID for python-mode."
+  "python")
+
+;; Configurar lsp-language-id-configuration para python-mode
+(setq lsp-language-id-configuration
+      '((python-mode . "python")))
+
+;; Configurar la función para el modo python
+(setq lsp-language-id-configuration
+      (append lsp-language-id-configuration
+              '((python-mode . my-lsp-python-language-id))))
+
+;;Configuracion de ruff-lsp
+;(require 'lsp)
+;(setq lsp-ruff-command "C:/Users/liuka/miniconda3/envs/spyder-env/Scripts/ruff.exe")
+;(add-hook 'python-mode-hook #'lsp)
+
+;;Configuración de pylsp
+(require 'lsp)
 (add-hook 'python-mode-hook #'lsp)
-(add-hook 'js2-mode-hook #'lsp)
 
-;; Configuración de Company Mode para trabajar con LSP
-(require 'company)
-
-;; Usar 'company-capf' como backend para completar con LSP
-(add-to-list 'company-backends 'company-capf)
-
-;; Asignar una tecla para la finalización de Company
-(global-set-key (kbd "C-c c") 'company-complete)
-
-(global-company-mode) ; Activa company-mode globalmente
-(add-hook 'after-init-hook 'global-company-mode) ; Habilita company-mode en todos los buffers
-(setq company-idle-delay 0.2)                    ; Retraso antes de mostrar sugerencias
-(setq company-minimum-prefix-length 1)            ; Mínimo de caracteres para mostrar sugerencias
-
-;; Agregar Company LSP al backend de Company Mode
-(add-to-list 'company-backends 'company-lsp)
-
-;; Asignar una tecla para la finalización de Company
-(global-set-key (kbd "C-c c") 'company-complete)
 
 ;; Establece una fuente personalizada (opcional)
 (set-frame-font "Fira Code Retina-14" nil t)
@@ -170,6 +225,10 @@
 ;; Atajo para cerrar Emacs
 (global-set-key (kbd "C-x C-c") 'save-buffers-kill-terminal)
 
+
+;; Atajo para ingresar a ibuffer
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
 ;; Iniciar Emacs en pantalla completa
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
@@ -182,7 +241,57 @@
 ;; Asigna la función a una combinación de teclas
 (global-set-key (kbd "C-c r") 'open-recent)
 
+;; Ajusta la ruta del binario de Cygwin
+(setq exec-path (append exec-path '("c:/cygwin64/bin")))
+
+;; Establece la shell para Cygwin
+(setq shell-file-name "bash")
+(setq explicit-shell-file-name shell-file-name)
+(setq explicit-bash.exe-args '("--noediting" "--login" "-i"))
+
+(add-hook 'c-mode-hook
+         (lambda ()
+           (setq compile-command (concat "gcc -mwindows -Wall -o " (file-name-sans-extension (buffer-file-name)) " " (buffer-file-name)))
+            (define-key c-mode-map (kbd "C-c C-c") 'compile)))
+
+(add-hook 'c++-mode-hook
+          (lambda ()
+           (setq compile-command (concat "g++ -mwindows -Wall -o " (file-name-sans-extension (buffer-file-name)) " " (buffer-file-name)))
+            (define-key c++-mode-map (kbd "C-c C-c") 'compile)))
+
+;; Instala y configura corfu
+(use-package corfu
+  :ensure t
+  :custom
+  (corfu-cycle t)
+  (corfu-auto t)
+  (corfu-auto-prefix 2)
+  (corfu-auto-delay 0.0)
+  (corfu-quit-at-bundary 'separator)
+  (corfu-echo-documentation 0.1)
+  (corfu-preview-current 'insert)
+  (corfu-preselect-first t)
+  :init
+  (global-corfu-mode)
+  (corfu-history-mode)
+  :config
+  ;; Configuración adicional si es necesaria
+  )
+
+;; Establece la posición de la ventana de selección de Corfu
+(setq corfu-cycle t)
+
+;; Establece la ubicación de la ventana de selección de Corfu
+(setq corfu-echo-documentation t)
+
+;; Teclas para seleccionar candidatos
+(define-key corfu-map (kbd "<tab>") 'corfu-next)
+(define-key corfu-map (kbd "<backtab>") 'corfu-previous)
 
 
-
-
+;;Codificación predeterminada
+;; Establece Latin-1 como codificación predeterminada para los buffers
+(prefer-coding-system 'latin-1)
+(set-default-coding-systems 'latin-1)
+(set-terminal-coding-system 'latin-1)
+(set-keyboard-coding-system 'latin-1)
